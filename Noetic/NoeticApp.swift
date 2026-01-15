a0cb6c1bcf12b491c0a9bc931ffc2d4ceb2fc868
saverio-negro <saverionegro@gmail.com>
2026-01-15 17:33:03 +0100

    Initial Commit

---
+++ Noetic/NoeticApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoeticApp.swift
+//  Noetic
+//
+//  Created by Saverio Negro on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct NoeticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoeticTests/NoeticTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoeticTests.swift
+//  NoeticTests
+//
+//  Created by Saverio Negro on 1/15/26.
+//
+
+import Testing
+@testable import Noetic
+
+struct NoeticTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
