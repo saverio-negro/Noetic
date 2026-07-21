35c9c924e332f26db3e6a017dc29b922177c9912
Saverio Negro <saverionegro@gmail.com>
2026-07-22 00:21:02 +0200

    Initial Commit

---
+++ Noetic/NoeticApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoeticApp.swift
+//  Noetic
+//
+//  Created by Saverio Negro on 7/22/26.
+//
+
+import SwiftUI
+
+@main
+struct NoeticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
